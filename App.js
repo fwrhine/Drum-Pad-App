657a174557c854615f752feb9baf05586c48a2d9
fwrhine <aghniaputriprawira@gmail.com>
2020-11-25 01:03:10 +1000

    Initial commit.

--- App.js
+++ App.js
@@ -1,21 +1,121 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const drumKit = {
+  "bass1": require("./assets/drum_sounds/bass1.mp3"),
+  "bass2": require("./assets/drum_sounds/bass2.mp3"),
+  "bass3": require("./assets/drum_sounds/bass3.mp3"),
+  "cymbal1": require("./assets/drum_sounds/cymbal1.mp3"),
+  "cymbal2": require("./assets/drum_sounds/cymbal2.mp3"),
+  "drumstick": require("./assets/drum_sounds/drumstick.mp3"),
+  "hihat": require("./assets/drum_sounds/hihat.mp3"),
+  "snare": require("./assets/drum_sounds/snare.mp3"),
+  "tom": require("./assets/drum_sounds/tom.mp3"),
+}
+
+const colors = {
+  "bass": "#6CC2BD",
+  "cymbal": "#5A809E",
+  "drumstick": "#7C79A2",
+  "hihat": "#F57D7C",
+  "snare": "#FFC1A6",
+  "tom": "#FEE4C4",
+}
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.rowContainer}>
+          <TouchableOpacity
+            onPress={()=>{}}
+            style={[{
+              backgroundColor: colors["bass"]
+            }, styles.button]}>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={()=>{}}
+            style={[{
+              backgroundColor: colors["bass"]
+            }, styles.button]}>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={()=>{}}
+            style={[{
+              backgroundColor: colors["bass"]
+            }, styles.button]}>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.rowContainer}>
+
+        <TouchableOpacity
+            onPress={()=>{}}
+            style={[{
+              backgroundColor: colors["cymbal"]
+            }, styles.button]}>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={()=>{}}
+            style={[{
+              backgroundColor: colors["cymbal"]
+            }, styles.button]}>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={()=>{}}
+            style={[{
+              backgroundColor: colors["drumstick"]
+            }, styles.button]}>
+          </TouchableOpacity>
+
+        </View>
+        <View style={styles.rowContainer}>
+
+        <TouchableOpacity
+            onPress={()=>{}}
+            style={[{
+              backgroundColor: colors["hihat"]
+            }, styles.button]}>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={()=>{}}
+            style={[{
+              backgroundColor: colors["snare"]
+            }, styles.button]}>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={()=>{}}
+            style={[{
+              backgroundColor: colors["tom"]
+            }, styles.button]}>
+          </TouchableOpacity>
+
+        </View>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 10,
+    marginTop: 100,
   },
+  rowContainer: {
+    flexDirection: "row"
+  },
+  button: {
+    flex: 1,
+    height: 100,
+    margin: 10,
+    borderRadius: 10
+  }
 });
